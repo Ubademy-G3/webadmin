1a66795511ae9991305f8145bed94b83471e4276
Daniela Leloutre <daniela@woowup.com>
2021-12-01 23:49:31 -0300

    dejo listo el layout y rutas

---
+++ src/presentation/transactions/Transactions.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function Transactions() {
+  return (
+    <>
+      <Typography component="p" variant="h4">
+        Transacciones
+      </Typography>
+    </>
+  );
+}
